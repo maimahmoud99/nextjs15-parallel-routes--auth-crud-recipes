a6d4de45cb2f3da482777134b765365c5527de05
Mai Mahmud <maimahmoudha99@gmail.com>
2025-06-19 17:38:30 +0300

    Update layout.js

--- app/recipes/layout.js
+++ app/recipes/layout.js
@@ -1,3 +1,13 @@
-export default function RecipesLayout({ children }) {
-  return <>{children}</>;
+// export default function RecipesLayout({ children }) {
+//   return <>{children}</>;
+// }
+export default function RecipesLayout({ children, featured, tags, recent }) {
+  return (
+    <>
+      {featured}
+      {tags}
+      {recent}
+      {children}
+    </>
+  );
 }
