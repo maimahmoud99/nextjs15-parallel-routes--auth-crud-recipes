90b7bbad7ae62c2eb4cefd807f65d65968cb283e
Mai Mahmud <maimahmoudha99@gmail.com>
2025-06-19 17:57:45 +0300

    Update layout.js

--- app/recipes/layout.js
+++ app/recipes/layout.js
@@ -1,20 +1,10 @@
-export default function RecipesLayout({
-  children,
-  featured,
-  tags,
-  recent,
-}: {
-  children: React.ReactNode;
-  featured: React.ReactNode;
-  tags: React.ReactNode;
-  recent: React.ReactNode;
-}) {
+export default function RecipesLayout({ children, featured, tags, recent }) {
   return (
-    <div>
+    <>
       {featured}
       {tags}
       {recent}
       {children}
-    </div>
+    </>
   );
 }
